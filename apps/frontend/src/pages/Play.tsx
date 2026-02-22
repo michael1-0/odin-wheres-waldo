1a3891f11571d58cac12b8ee8be6fef99b61700e
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2026-02-22 17:49:45 +0700

    Fix z-index loading state issue

--- apps/frontend/src/pages/Play.tsx
+++ apps/frontend/src/pages/Play.tsx
@@ -552,8 +552,8 @@ function Play() {
           )}
         {isLoading &&
           createPortal(
-            <div className="fixed inset-0 bg-white/55 flex items-center justify-center z-9999">
-              <div className="px-6 py-4 text-3xl bg-white rounded-sm">
+            <div className="fixed inset-0 bg-white/55 flex items-center justify-center z-99999">
+              <div className="px-6 py-4 text-3xl bg-white rounded-sm shadow-lg">
                 Loading...
               </div>
             </div>,
