7ce2e4fe52099af68d29b0d80fb74e41a3f4c43f
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2026-02-22 12:48:44 +0700

    Modify not found page content

--- apps/frontend/src/pages/NotFound.tsx
+++ apps/frontend/src/pages/NotFound.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router";
+
 function NotFound() {
-  return <div>not found</div>;
+  return (
+    <div className="flex items-center w-full flex-col justify-center">
+      <div className="min-w-full text-center mb-10 font-bold">Page Not Found</div>
+      <Link to={"/"} className="underline">Go back home</Link>
+    </div>
+  );
 }
 
 export default NotFound;
